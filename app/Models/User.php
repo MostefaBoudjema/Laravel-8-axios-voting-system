1566e25f64814eea64bb0fa88ee7804910c84d1e
mostfa <myreal17@gmail.com>
2022-08-29 22:04:37 +0100

    static

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function maxi()
+    public static function maxi()
     {
         // return 55;
         return User::max('id')+1;
