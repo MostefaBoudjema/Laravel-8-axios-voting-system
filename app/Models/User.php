38939b36fad38ff02a785ddf99dc5277142c7c90
mostfa <myreal17@gmail.com>
2022-08-31 13:26:49 +0100

    static rolename

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,7 +70,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Vote::class);
     }
-    public function RoleName($id)
+    public static function RoleName($id)
     {
         if($id==0) return 'user';
         else if ($id==1) return 'Admin';
